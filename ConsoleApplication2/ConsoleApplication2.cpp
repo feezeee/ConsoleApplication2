f19d443cc1d0917d9d03dbcc7567076274c61946
feezeee <45011033+feezeee@users.noreply.github.com>
2020-05-11 23:36:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+const double a = -2;
+const double b = 3;
+
+double calculateValue(double x)
+{
+	double rezult = 4 * x - 7 * sin(x);
+	return rezult;
+}
+
+int main()
+{
+	setlocale(0, "");
+	int n;
+	cout << "Введите колличество разбиений: "; cin >> n;
+	double h = (b-a )/ n;
+	double first = a;
+	double rezult = 0;
+	
+	for(int i=0;i<n;i++)
+	{		
+		rezult += calculateValue(first+h/2+i*h);		
+	}
+	cout << "Ответ: " << h* rezult;
+}
+
